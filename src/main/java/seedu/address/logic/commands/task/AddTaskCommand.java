245b526378be611cdce76ea76aee4e6266db903b
simonteozw <simonteozw@gmail.com>
2020-10-08 02:38:37 +0800

    Set up Task command package structure

---
+++ src/main/java/seedu/address/logic/commands/task/AddTaskCommand.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.commands.task;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.task.Task;
+
+public class AddTaskCommand extends Command {
+    public static final String COMMAND_WORD = "add";
+
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the address book";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddContactCommand to add the specified {@code Contact}
+     */
+    public AddTaskCommand(Task task) {
+        requireNonNull(task);
+        toAdd = task;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasTask(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
+        }
+
+        model.addTask(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddTaskCommand // instanceof handles nulls
+                && toAdd.equals(((AddTaskCommand) other).toAdd));
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/task/AddTaskCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser.task;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.task.AddTaskCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AddTaskCommandParser implements Parser<AddTaskCommand> {
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddContactCommand
+     * and returns an AddContactCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddTaskCommand parse(String args) throws ParseException {
+
+        return null;
+    }
+}
