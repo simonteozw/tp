650a4ed6c3cbd78aece0f1baa7c6e5ad070d122e
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-10-10 22:46:45 +0800

    Fix a bug in Module Code's regex and add some testcases for it

---
+++ src/test/java/seedu/address/logic/parser/module/AddModuleCommandParserTest.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser.module;
+
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.module.AddModuleCommand;
+import seedu.address.model.commons.Code;
+import seedu.address.model.commons.Name;
+import seedu.address.model.module.Module;
+
+public class AddModuleCommandParserTest {
+    private final AddModuleCommandParser parser = new AddModuleCommandParser();
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        AddModuleCommand expectedCommand = new AddModuleCommand(new Module(new Code("CS1231S"), new Name("Discrete " +
+            "Structures"), "Introductory mathematical tools required for computer science"));
+
+        assertParseSuccess(parser, " n/Discrete Structures m/CS1231S d/Introductory mathematical" +
+            " " +
+            "tools " +
+            "required" + " " + "for " + "computer science", expectedCommand);
+    }
+
+}
---
+++ src/test/java/seedu/address/model/commons/CodeTest.java
@@ -0,0 +1,18 @@
+package seedu.address.model.commons;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CodeTest {
+
+    @Test
+    void isValidCode() {
+        assertTrue(Code.isValidCode("CS1231S"));
+        assertTrue(Code.isValidCode("MA1101R"));
+        assertTrue(Code.isValidCode("MA1102"));
+        assertTrue(Code.isValidCode("CFG1101"));
+        assertFalse(Code.isValidCode("A3BBBB"));
+    }
+}
