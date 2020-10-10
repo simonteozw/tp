21b65cad72c6fb83639c71d31aa35573cfbd2668
Minh Nguyen <nguyen.dq.minh@gmail.com>
2020-10-11 02:14:29 +0800

    Fix command format according to the UG

--- src/main/java/seedu/address/logic/commands/lesson/DeleteLessonCommand.java
+++ src/main/java/seedu/address/logic/commands/lesson/DeleteLessonCommand.java
@@ -2,15 +2,17 @@ package seedu.address.logic.commands.lesson;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.commons.Code;
 import seedu.address.model.lesson.Lesson;
+import seedu.address.model.lesson.Type;
+
+import java.util.Optional;
 
 public class DeleteLessonCommand extends Command {
 
@@ -21,22 +23,25 @@ public class DeleteLessonCommand extends Command {
 
     public static final String MESSAGE_DELETE_LESSON_SUCCESS = "Deleted Lesson: %1$s";
 
-    private final Index targetIndex;
+    private final Code targetCode;
+    private final Type targetType;
 
-    public DeleteLessonCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteLessonCommand(Code code, Type type) {
+        this.targetCode = code;
+        this.targetType = type;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Lesson> lastShownList = model.getFilteredLessonList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
+        Optional<Lesson> requestedLesson = model.getLesson(targetCode, targetType);
+
+        if (requestedLesson.isEmpty()) {
+            throw new CommandException(Messages.MESSAGE_LESSON_DOES_NOT_EXIST);
         }
 
-        Lesson lessonToDelete = lastShownList.get(targetIndex.getZeroBased());
+        Lesson lessonToDelete = requestedLesson.get();
         model.deleteLesson(lessonToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_LESSON_SUCCESS, lessonToDelete));
     }
@@ -45,6 +50,7 @@ public class DeleteLessonCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteLessonCommand // instanceof handles nulls
-                && targetIndex.equals(((DeleteLessonCommand) other).targetIndex)); // state check
+                && targetCode.equals(((DeleteLessonCommand) other).targetCode)
+                && targetType.equals(((DeleteLessonCommand) other).targetType)); // state check
     }
 }
