24fca3cb3636ac5ba5d62780f46f87b31def0d9f
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-10-10 17:13:52 +0800

    Refactor Code class to model.commons for Lesson's use

--- src/main/java/seedu/address/model/commons/Code.java
+++ src/main/java/seedu/address/model/commons/Code.java
@@ -1,4 +1,4 @@
-package seedu.address.model.module;
+package seedu.address.model.commons;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
--- src/main/java/seedu/address/model/module/Module.java
+++ src/main/java/seedu/address/model/module/Module.java
@@ -4,6 +4,7 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Objects;
 
+import seedu.address.model.commons.Code;
 import seedu.address.model.commons.Name;
 
 /**
