18381bdb1a36e1e5d0ca917594d9646b40b0c8d2
simonteozw <simonteozw@gmail.com>
2020-10-11 16:26:14 +0800

    Refactor ViewTaskCommand to take in index instead of search keywords

--- src/main/java/seedu/address/logic/commands/task/ViewTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/task/ViewTaskCommand.java
@@ -2,11 +2,14 @@ package seedu.address.logic.commands.task;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.task.NameContainsKeywordsPredicate;
 import seedu.address.model.task.Task;
 
 public class ViewTaskCommand extends Command {
@@ -14,29 +17,34 @@ public class ViewTaskCommand extends Command {
     public static final String COMMAND_WORD = "info";
 
     public static final String MESSAGE_USAGE = Task.TYPE + " " + COMMAND_WORD
-        + ": Finds all tasks where the name contains any of "
-        + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-        + "Example: " + COMMAND_WORD + " CS2103T CS2100";
+        + ": Views the info of the task with the specified index.\n"
+        + "Example: " + Task.TYPE + " " + COMMAND_WORD + " 1";
+    private static final String MESSAGE_VIEW_TASK_SUCCESS = "Here is the task you requested: %1$s";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final Index targetIndex;
 
-    public ViewTaskCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    public ViewTaskCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredTaskList(predicate);
-        return new CommandResult(
-            String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, model.getFilteredTaskList().size()));
+        List<Task> lastShownList = model.getFilteredTaskList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        Task taskToShow = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteTask(taskToShow);
+        return new CommandResult(String.format(MESSAGE_VIEW_TASK_SUCCESS, taskToShow));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
             || (other instanceof ViewTaskCommand // instanceof handles nulls
-            && predicate.equals(((ViewTaskCommand) other).predicate)); // state check
+            && targetIndex.equals(((ViewTaskCommand) other).targetIndex)); // state check
     }
 }
--- src/main/java/seedu/address/logic/parser/task/ViewTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/task/ViewTaskCommandParser.java
@@ -2,12 +2,11 @@ package seedu.address.logic.parser.task;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
-
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.task.ViewTaskCommand;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.task.NameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new ViewTaskCommand object
@@ -16,19 +15,17 @@ public class ViewTaskCommandParser implements Parser<ViewTaskCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindContactCommand
-     * and returns a FindContactCommand object for execution.
+     * and returns a ViewTaskCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewTaskCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ViewTaskCommand(index);
+        } catch (ParseException pe) {
             throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewTaskCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewTaskCommand.MESSAGE_USAGE), pe);
         }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new ViewTaskCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 }
