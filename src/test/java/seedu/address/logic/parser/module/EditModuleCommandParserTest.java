5eebf84de31e1dfa66af27bf811ba65ad4fffc11
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-10-14 10:29:23 +0800

    Refactor Contact-specific test util to ContactCommandTestUtil
    This is to prepare for the addition of testcases for newly added modules

--- src/test/java/seedu/address/logic/parser/module/EditModuleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/module/EditModuleCommandParserTest.java
@@ -11,6 +11,13 @@ public class EditModuleCommandParserTest {
     private final EditModuleCommandParser parser = new EditModuleCommandParser();
 
     @Test
+    public void parse_allFieldsPresent_success() {
+//        EditModuleCommand expected = new EditModuleCommand(new Code("CS1231S"),new EditModuleCommand
+//        .EditModuleDescriptor())
+//        assertParseSuccess(parser, "m/CS1231S n/Sample d/Sample", new Edi);
+    }
+
+    @Test
     public void parse_noFieldsProvided_failure() {
         assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
             EditModuleCommand.MESSAGE_USAGE));
