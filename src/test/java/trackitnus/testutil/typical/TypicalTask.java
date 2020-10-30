89fa64aeb59659bb83848e6cf55cbe943c632ee1
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-10-30 23:15:31 +0800

    Merge TypicalData with SampleDataUtil
    The rationale behind this is to unify the internal & external test data since these 2 testsets should have the same properties

--- src/test/java/trackitnus/testutil/typical/TypicalTask.java
+++ src/test/java/trackitnus/testutil/typical/TypicalTask.java
@@ -1,27 +1,17 @@
 package trackitnus.testutil.typical;
 
-import static trackitnus.testutil.typical.TypicalDates.DATE_ONE;
-import static trackitnus.testutil.typical.TypicalDates.DATE_THREE;
-import static trackitnus.testutil.typical.TypicalDates.DATE_TWO;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import trackitnus.model.task.Task;
+import trackitnus.model.util.SampleDataUtil;
 
 public class TypicalTask {
-
-    public static final Task TASK_FULL = new Task(TypicalNames.get(0), DATE_ONE, TypicalModule.getOneCode(0),
-        "Sample Remark");
-    public static final Task TASK_FULL_ANOTHER = new Task(TypicalNames.get(1), DATE_TWO, TypicalModule.getOneCode(1),
-        "Sample Remark");
-    public static final Task TASK_NO_REMARK = new Task(TypicalNames.get(2), DATE_THREE, TypicalModule.getOneCode(2),
-        "");
-    public static final Task TASK_NO_CODE = new Task(TypicalNames.get(1), DATE_THREE, null,
-        "");
-
     public static List<Task> get() {
-        return new ArrayList<>(Arrays.asList(TASK_FULL, TASK_FULL_ANOTHER, TASK_NO_REMARK, TASK_NO_CODE));
+        return Arrays.asList(SampleDataUtil.getSampleTasks());
+    }
+
+    public static Task get(int index) {
+        return get().get(index);
     }
 }
