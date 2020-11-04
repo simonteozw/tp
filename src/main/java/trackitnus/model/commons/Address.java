2282ce7e484f27a8a8dd439a2fad0a78455ca045
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-11-05 01:13:38 +0800

    Limit the length of address to 20

--- src/main/java/trackitnus/model/commons/Address.java
+++ src/main/java/trackitnus/model/commons/Address.java
@@ -10,13 +10,14 @@ import trackitnus.commons.util.AppUtil;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Address's length must be between 1 and 20";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[^\\s].*";
+    private static final int LENGTH_LIMIT = 20;
 
     public final String value;
 
@@ -35,6 +36,9 @@ public class Address {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidAddress(String test) {
+        if (test.length() > LENGTH_LIMIT) {
+            return false;
+        }
         return test.matches(VALIDATION_REGEX);
     }
 
