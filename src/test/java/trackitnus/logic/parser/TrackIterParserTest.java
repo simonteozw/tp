e661f2ca8bbb7cb5324b2f24aeb7582acb76dac6
simonteozw <simonteozw@gmail.com>
2020-11-06 15:47:09 +0800

    Add parser tests for task

--- src/test/java/trackitnus/logic/parser/TrackIterParserTest.java
+++ src/test/java/trackitnus/logic/parser/TrackIterParserTest.java
@@ -11,12 +11,19 @@ import trackitnus.logic.commands.HelpCommand;
 import trackitnus.logic.commands.contact.AddContactCommand;
 import trackitnus.logic.commands.contact.DeleteContactCommand;
 import trackitnus.logic.commands.contact.EditContactCommand;
+import trackitnus.logic.commands.task.AddTaskCommand;
+import trackitnus.logic.commands.task.DeleteTaskCommand;
+import trackitnus.logic.commands.task.EditTaskCommand;
 import trackitnus.logic.parser.exceptions.ParseException;
 import trackitnus.model.contact.Contact;
+import trackitnus.model.task.Task;
 import trackitnus.testutil.Assert;
 import trackitnus.testutil.ContactUtil;
+import trackitnus.testutil.TaskUtil;
 import trackitnus.testutil.builder.ContactBuilder;
 import trackitnus.testutil.builder.EditContactDescriptorBuilder;
+import trackitnus.testutil.builder.EditTaskDescriptorBuilder;
+import trackitnus.testutil.builder.TaskBuilder;
 import trackitnus.testutil.typical.TypicalIndexes;
 
 public class TrackIterParserTest {
@@ -47,6 +54,32 @@ public class TrackIterParserTest {
             + EditContactCommand.COMMAND_WORD + " " + TypicalIndexes.INDEX_FIRST.getOneBased() + " "
             + ContactUtil.getEditContactDescriptorDetails(descriptor));
         assertEquals(new EditContactCommand(TypicalIndexes.INDEX_FIRST, descriptor), command);
+    }
+
+    @Test
+    public void parseTaskCommand_add() throws Exception {
+        Task task = new TaskBuilder().build();
+        AddTaskCommand command = (AddTaskCommand) parser.parseCommand(Task.TYPE
+            + " " + TaskUtil.getAddCommand(task));
+        assertEquals(new AddTaskCommand(task), command);
+    }
+
+    @Test
+    public void parseTaskCommand_delete() throws Exception {
+        DeleteTaskCommand command = (DeleteTaskCommand) parser.parseCommand(
+            Task.TYPE + " " + DeleteTaskCommand.COMMAND_WORD + " " + TypicalIndexes.INDEX_FIRST
+                .getOneBased());
+        assertEquals(new DeleteTaskCommand(TypicalIndexes.INDEX_FIRST), command);
+    }
+
+    @Test
+    public void parseTaskCommand_edit() throws Exception {
+        Task task = new TaskBuilder().build();
+        EditTaskCommand.EditTaskDescriptor descriptor = new EditTaskDescriptorBuilder(task).build();
+        EditTaskCommand command = (EditTaskCommand) parser.parseCommand(Task.TYPE + " "
+            + EditTaskCommand.COMMAND_WORD + " " + TypicalIndexes.INDEX_FIRST.getOneBased() + " "
+            + TaskUtil.getEditTaskDescriptorDetails(descriptor));
+        assertEquals(new EditTaskCommand(TypicalIndexes.INDEX_FIRST, descriptor), command);
     }
 
     @Test
