bf0f030e566d7d54a5feb194f019da78f4e03eea
nottiffchan <nottiffchan@gmail.com>
2020-11-08 15:14:07 +0800

    Update test cases again

--- src/test/java/trackitnus/logic/commands/CommandResultTest.java
+++ src/test/java/trackitnus/logic/commands/CommandResultTest.java
@@ -29,14 +29,6 @@ public class CommandResultTest {
         // different types -> returns false
         assertFalse(commandResult.equals(0.5f));
 
-        // edited module -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback", false, false,
-            new Module(new Code("AA0000"), new Name("not edited")), new Module(new Code("AA0000"), new Name("after " +
-            "edited")), "")));
-
-        // deleted module -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback", false, false, null, null, "AA0000")));
-
         // different feedbackToUser value -> returns false
         assertFalse(commandResult.equals(new CommandResult("different")));
 
@@ -45,6 +37,14 @@ public class CommandResultTest {
 
         // different exit value -> returns false
         assertFalse(commandResult.equals(new CommandResult("feedback", false, true, null, null, "")));
+
+        // edited module -> returns true
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false,
+            new Module(new Code("AA0000"), new Name("not edited")), new Module(new Code("AA0000"), new Name("after " +
+            "edited")), "")));
+
+        // deleted module -> returns true
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, null, null, "AA0000")));
     }
 
     @Test
